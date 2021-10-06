e9ae4ecfb6c0b64c5ea9a364f4c718f16925279a
Cube219 <max804800@gmail.com>
2021-10-06 18:36:01 +0900

    Added LazySeg

--- content/data-structures/LazySegmentTree.h
+++ content/data-structures/LazySegmentTree.h
@@ -1,66 +1,64 @@
 /**
- * Author: Simon Lindholm
- * Date: 2016-10-08
- * License: CC0
- * Source: me
- * Description: Segment tree with ability to add or set values of large intervals, and compute max of intervals.
- * Can be changed to other things.
- * Use with a bump allocator for better performance, and SmallPtr or implicit indices to save memory.
+ * Author: max804
+ * Description: Lazy segment tree
  * Time: O(\log N).
- * Usage: Node* tr = new Node(v, 0, sz(v));
- * Status: stress-tested a bit
+ * Usage: sg.init(n); sg.initv(idx, v); sg.build();
+ *        sg.find(l, r); sg.add(l, r, v);
  */
 #pragma once
 
-#include "../various/BumpAllocator.h"
-
-const int inf = 1e9;
-struct Node {
-	Node *l = 0, *r = 0;
-	int lo, hi, mset = inf, madd = 0, val = -inf;
-	Node(int lo,int hi):lo(lo),hi(hi){} // Large interval of -inf
-	Node(vi& v, int lo, int hi) : lo(lo), hi(hi) {
-		if (lo + 1 < hi) {
-			int mid = lo + (hi - lo)/2;
-			l = new Node(v, lo, mid); r = new Node(v, mid, hi);
-			val = max(l->val, r->val);
-		}
-		else val = v[lo];
-	}
-	int query(int L, int R) {
-		if (R <= lo || hi <= L) return -inf;
-		if (L <= lo && hi <= R) return val;
-		push();
-		return max(l->query(L, R), r->query(L, R));
-	}
-	void set(int L, int R, int x) {
-		if (R <= lo || hi <= L) return;
-		if (L <= lo && hi <= R) mset = val = x, madd = 0;
-		else {
-			push(), l->set(L, R, x), r->set(L, R, x);
-			val = max(l->val, r->val);
-		}
-	}
-	void add(int L, int R, int x) {
-		if (R <= lo || hi <= L) return;
-		if (L <= lo && hi <= R) {
-			if (mset != inf) mset += x;
-			else madd += x;
-			val += x;
-		}
-		else {
-			push(), l->add(L, R, x), r->add(L, R, x);
-			val = max(l->val, r->val);
-		}
-	}
-	void push() {
-		if (!l) {
-			int mid = lo + (hi - lo)/2;
-			l = new Node(lo, mid); r = new Node(mid, hi);
-		}
-		if (mset != inf)
-			l->set(lo,hi,mset), r->set(lo,hi,mset), mset = inf;
-		else if (madd)
-			l->add(lo,hi,madd), r->add(lo,hi,madd), madd = 0;
-	}
+class SegTree
+{
+public:
+    vector<ll> t, lazy;
+    int stLeaf;
+    void init(int n) {
+        stLeaf = 1;
+        while(stLeaf < n) stLeaf *= 2;
+        t.resize(stLeaf * 2);
+        lazy.resize(stLeaf * 2);
+    }
+    void initv(int idx, ll v) {
+        t[stLeaf + idx] = v;
+    }
+    void build() {
+        for(int i = stLeaf - 1; i > 0; --i) {
+            t[i] = t[i * 2] + t[i * 2 + 1];
+        }
+    }
+    void push(int l, int r, int node) {
+        if(lazy[node] == 0) return;
+        if(node < stLeaf) {
+            lazy[node * 2] += lazy[node];
+            lazy[node * 2 + 1] += lazy[node];
+        }
+        t[node] += (r - l + 1) * lazy[node];
+        lazy[node] = 0;
+    }
+    ll findImpl(int cl, int cr, int l, int r, int node) {
+        push(cl, cr, node);
+        if(cr < l || r < cl) return 0;
+        if(l <= cl && cr <= r) return t[node];
+        int m = (cl + cr) / 2;
+        return findImpl(cl, m, l, r, node * 2) + findImpl(m + 1, cr, l, r, node * 2 + 1);
+    }
+    void addImpl(int cl, int cr, int l, int r, int node, ll v) {
+        push(cl, cr, node);
+        if(cr < l || r < cl) return;
+        if(l <= cl && cr <= r) {
+            lazy[node] += v;
+            push(cl, cr, node);
+            return;
+        }
+        int m = (cl + cr) / 2;
+        addImpl(cl, m, l, r, node * 2, v);
+        addImpl(m + 1, cr, l, r, node * 2 + 1, v);
+        t[node] = t[node * 2] + t[node * 2 + 1];
+    }
+    ll find(int l, int r) {
+        return findImpl(0, stLeaf - 1, l, r, 1);
+    }
+    void add(int l, int r, ll v) {
+        addImpl(0, stLeaf - 1, l, r, 1, v);
+    }
 };
