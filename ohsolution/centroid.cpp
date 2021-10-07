c2c50623905155a53a2c0da12ea300637d545b40
ohhaeseong <43700074+ohsolution@users.noreply.github.com>
2021-10-07 18:25:31 +0900

    Update centroid.cpp

--- ohsolution/centroid.cpp
+++ ohsolution/centroid.cpp
@@ -3,8 +3,8 @@ vector<pair<int,int>> adj[max_v]; // nxt, dist pair
 int vist[max_v],sz[max_v];
 int cp[max_v]; // centroid tree parent
 
-int dfsz(int u, int par = -1) // caution : hld 를 같이 사용할 때 hld 에서 사용하는 sz배열과 겹치지 않게 
-{
+int dfsz(int u, int par = -1) // caution: when using hld together, it must not overlap with the sz array used in hld
+{	
 	sz[u] = 1;
 	for (auto& v : adj[u]) if (v.first != par && !vist[v.first]) sz[u] += dfsz(v.first, u);
 	return sz[u];
@@ -22,9 +22,11 @@ void go(int u,int trp)
 	int cen = fc(u, csz/2); // find centroid
 
 	vist[cen] = 1;
-	cp[cen] = trp; // 해당 센트에 부모 센트로이드 설정 make centroid tree
+	cp[cen] = trp; // setting parent centroid of cur cen
 
-	vector<int> cur; // 센트로이드를 단절점으로 하는 여러개에 서브트리들의 정보를 map으로 모은후 merge 를 logn 에 할 수 있다.
+	vector<int> cur; 
+	// After collecting the information of the subtrees into a map in several places with centroid as a disconnect point,
+	// the merge can be performed on the logn.
 
 	function<void(int,int)> getsub = [&](int u,int par)
 	{
@@ -42,9 +44,9 @@ void go(int u,int trp)
 	for (auto& v : adj[cen]) if (!vist[v.first]) go(v.first,cen); // go nxt centroid
 }
 
-// 한 흰색정점 v가 주어질 때 다른 흰색정점과의 최단거리
+// When given a white vertex v, the shortest distance from the other vertex.
 int color[max_v];
-multiset <int> xset[max_v]; // 해당 정점으로부터 흰색정점들의 거리를 모은 set
+multiset <int> xset[max_v]; // The set that collects the distances of the white vertices from the vertex.
 int p[20][max_v],d[max_v];
 
 int getdist(int u, int v)
@@ -52,7 +54,7 @@ int getdist(int u, int v)
 	return d[u] + d[v] - 2 * d[lca(u, v)];
 }
 
-void upd(int v) // 센트로이드 트리 update v번 노드의 색깔을 바꾼다.
+void upd(int v) // Change the color of node v of the centroid tree update.
 {	
 	color[v] = !color[v];
 	int i = v;
@@ -60,21 +62,21 @@ void upd(int v) // 센트로이드 트리 update v번 노드의 색깔을 바꾼
 	while (~i)
 	{	
 		int dist = getdist(i, v);
-		if (color[v]) xset[i].insert(dist); // 흰색이면 ins
-		else xset[i].erase(xset[i].find(dist)); // 검은색으로 바꼈으면 erase
-		i = cp[i]; // 해당 센트 부모로 이동
-}
+		if (color[v]) xset[i].insert(dist); // if color is white than ins
+		else xset[i].erase(xset[i].find(dist)); // if changed color is black then erase
+		i = cp[i]; // move to parent cent
+	}
 }
 
-int query(int v) // 센트로이드 트리 query 검은색 정점 v와 최단거리인 흰색정점 구하기
+int query(int v) // Centroid tree query. Find the black vertex v and the shortest white vertex.
 {
 	int i = v;
 	int ret = INF;
 	int ct = 0;
 	while (~i)
 	{		
-		int dist = getdist(i, v); // v와 현재 센트와의 거리
-		if (xset[i].size()) ckmin(ret, dist + *xset[i].begin()); // 셋에 저장되어있는 최소 흰색정점 거리
+		int dist = getdist(i, v); // distance with current cent to v
+		if (xset[i].size()) ckmin(ret, dist + *xset[i].begin()); // saved white point distance
 		i = cp[i];
 	}
 
