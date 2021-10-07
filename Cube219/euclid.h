e371f06f56138802c2e7c72ce69c3d5381d0dbb4
Cube219 <max804800@gmail.com>
2021-10-07 17:48:53 +0900

    Added euclid (Added general sol)

---
+++ Cube219/euclid.h
@@ -0,0 +1,16 @@
+/**
+ * Author: Unknown
+ * Date: 2002-09-15
+ * Source: predates tinyKACTL
+ * Description: Finds two integers $x$ and $y$, such that $ax+by=\gcd(a,b)$. If
+ * you just need gcd, use the built in \texttt{\_\_gcd} instead.
+ * If $a$ and $b$ are coprime, then $x$ is the inverse of $a \pmod{b}$.
+ * $x2 = x + k * b/gcd(a,b) y2 = y - k * a/gcd(a,b)$
+ */
+#pragma once
+
+ll euclid(ll a, ll b, ll &x, ll &y) {
+	if (!b) return x = 1, y = 0, a;
+	ll d = euclid(b, a % b, y, x);
+	return y -= a/b * x, d;
+}
