7176d87363e735e36113c6d92b4dbd0a78dbc002
Cube219 <max804800@gmail.com>
2021-10-07 23:59:47 +0900

    Update Numerical and Number Theory

--- content/number-theory/FastEratosthenes.h
+++ content/number-theory/FastEratosthenes.h
@@ -1,39 +1,28 @@
 /**
- * Author: Jakob Kogler, chilli, pajenegod
- * Date: 2020-04-12
+ * Author: ohsolution
+ * Date: 2021-10-07
  * License: CC0
  * Description: Prime sieve for generating all primes smaller than LIM.
- * Time: LIM=1e9 $\approx$ 1.5s
- * Status: Stress-tested
- * Details: Despite its n log log n complexity, segmented sieve is still faster
- * than other options, including bitset sieves and linear sieves. This is
- * primarily due to its low memory usage, which reduces cache misses. This
- * implementation skips even numbers.
- *
- * Benchmark can be found here: https://ideone.com/e7TbX4
- *
- * The line `for (int i=idx; i<S+L; idx = (i += p))` is done on purpose for performance reasons.
- * Se https://github.com/kth-competitive-programming/kactl/pull/166#discussion_r408354338
+ * Time: O(N)
  */
 #pragma once
 
 const int LIM = 1e6;
-bitset<LIM> isPrime;
-vi eratosthenes() {
-	const int S = (int)round(sqrt(LIM)), R = LIM / 2;
-	vi pr = {2}, sieve(S+1); pr.reserve(int(LIM/log(LIM)*1.1));
-	vector<pii> cp;
-	for (int i = 3; i <= S; i += 2) if (!sieve[i]) {
-		cp.push_back({i, i * i / 2});
-		for (int j = i * i; j <= S; j += 2 * i) sieve[j] = 1;
+vector<int> pr; // prime set
+int sp[LIM]; // minimum prime
+int cnt[LIM]; // 2 ^ (prime_num)
+int mu[LIM];
+
+void get_sieve() { 
+	cnt[1] = 1;
+	for(int i = 2; i<LIM;++i) {
+		if (!sp[i]) pr.push_back(i), cnt[i] = 2, mu[i] = -1;
+		for (auto& x : pr) {
+			if (x * i >= LIM) break;
+			sp[x * i] = x;
+			cnt[x * i] = i % x == 0 ? cnt[i] : cnt[i]+1;
+			mu[x * i] = (i % x != 0) * (-mu[i]);
+			if (i % x == 0) break;
+		}
 	}
-	for (int L = 1; L <= R; L += S) {
-		array<bool, S> block{};
-		for (auto &[p, idx] : cp)
-			for (int i=idx; i < S+L; idx = (i+=p)) block[i-L] = 1;
-		rep(i,0,min(S, R - L))
-			if (!block[i]) pr.push_back((L + i) * 2 + 1);
-	}
-	for (int i : pr) isPrime[i] = 1;
-	return pr;
-}
+};
