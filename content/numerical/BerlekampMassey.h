093ba6fd3e0c6f2a1f430aa7005bf356d3916b4a
ohhaeseong <43700074+ohsolution@users.noreply.github.com>
2022-11-08 16:12:05 +0900

    Update BerlekampMassey.h

--- content/numerical/BerlekampMassey.h
+++ content/numerical/BerlekampMassey.h
@@ -1,71 +1,82 @@
-/**
- * Author: palilo
- * Date: 2021-10-08
- * Description: 
- * Usage: 
- */
-#pragma once
+using lint = long long;
 
-template <typename num>
-std::vector<num> BerlekampMassey(const std::vector<num>& s) {
-	int n = int(s.size()), L = 0, m = 0;
-	std::vector<num> C(n), B(n), T;
-	C[0] = B[0] = 1;
-	num b = 1;
-	for (int i = 0; i < n; i++) {
-		++m;
-		num d = s[i];
-		for (int j = 1; j <= L; j++) d += C[j] * s[i - j];
-		if (d == 0) continue;
-		T = C;
-		num coef = d / b;
-		for (int j = m; j < n; j++) C[j] -= coef * B[j - m];
-		if (2 * L > i) continue;
-		L = i + 1 - L;
-		B = T;
-		b = d;
-		m = 0;
+lint ipow(lint x, lint p) {
+	lint ret = 1, piv = x;
+	while (p) 
+	{
+		if (p & 1) ret = ret * piv % mod;
+		piv = piv * piv % mod;
+		p >>= 1;
 	}
-	C.resize(L + 1);
-	C.erase(C.begin());
-	for (auto& x : C) {
-		x = -x;
-	}
-	return C;
+	return ret;
 }
-
-template <typename num>
-num linearRec(const std::vector<num>& S, const std::vector<num>& tr, long long k) {
-	int n = int(tr.size());
-	assert(S.size() >= tr.size());
-	auto combine = [&](std::vector<num> a, std::vector<num> b) {
-		std::vector<num> res(n * 2 + 1);
-		for (int i = 0; i <= n; i++)
-			for (int j = 0; j <= n; j++)
-				res[i + j] += a[i] * b[j];
-		for (int i = 2 * n; i > n; --i)
-			for (int j = 0; j < n; j++)
-				res[i - 1 - j] += res[i] * tr[j];
-		res.resize(n + 1);
-		return res;
+vector<int> berlekamp_massey(vector<int> x) {
+	vector<int> ls, cur;
+	int lf, d;
+	for (int i = 0; i < x.size(); i++) {
+		lint t = 0;
+		for (int j = 0; j < cur.size(); j++) {
+			t = (t + 1ll * x[i - j - 1] * cur[j]) % mod;
+		}
+		if ((t - x[i]) % mod == 0) continue;
+		if (cur.empty()) {
+			cur.resize(i + 1);
+			lf = i;
+			d = (t - x[i]) % mod;
+			continue;
+		}
+		lint k = -(x[i] - t) * ipow(d, mod - 2) % mod;
+		vector<int> c(i - lf - 1);
+		c.push_back(k);
+		for (auto& j : ls) c.push_back(-j * k % mod);
+		if (c.size() < cur.size()) c.resize(cur.size());
+		for (int j = 0; j < cur.size(); j++) {
+			c[j] = (c[j] + cur[j]) % mod;
+		}
+		if (i - lf + (int)ls.size() >= (int)cur.size()) {
+			tie(ls, lf, d) = make_tuple(cur, i, (t - x[i]) % mod);
+		}
+		cur = c;
+	}
+	for (auto& i : cur) i = (i % mod + mod) % mod;
+	return cur;
+}
+int get_nth(vector<int> rec, vector<int> dp, lint n) {
+	int m = rec.size();
+	vector<int> s(m), t(m);
+	s[0] = 1;
+	if (m != 1) t[1] = 1;
+	else t[0] = rec[0];
+	auto mul = [&rec](vector<int> v, vector<int> w) {
+		int m = v.size();
+		vector<int> t(2 * m);
+		for (int j = 0; j < m; j++) {
+			for (int k = 0; k < m; k++) {
+				t[j + k] += 1ll * v[j] * w[k] % mod;
+				if (t[j + k] >= mod) t[j + k] -= mod;
+			}
+		}
+		for (int j = 2 * m - 1; j >= m; j--) {
+			for (int k = 1; k <= m; k++) {
+				t[j - k] += 1ll * t[j] * rec[k - 1] % mod;
+				if (t[j - k] >= mod) t[j - k] -= mod;
+			}
+		}
+		t.resize(m);
+		return t;
 	};
-	std::vector<num> pol(n + 1), e(pol);
-	pol[0] = e[1] = 1;
-	for (++k; k; k /= 2) {
-		if (k % 2) pol = combine(pol, e);
-		e = combine(e, e);
+	while (n) {
+		if (n & 1) s = mul(s, t);
+		t = mul(t, t);
+		n >>= 1;
 	}
-	num res = 0;
-	for (int i = 0; i < n; i++)
-		res += pol[i + 1] * S[i];
-	return res;
+	lint ret = 0;
+	for (int i = 0; i < m; i++) ret += 1ll * s[i] * dp[i] % mod;
+	return ret % mod;
 }
-
-TEST_CASE("Berlekamp Massey", "[bm]") {
-	using num = modnum<int(1e9) + 7>;
-	vector<num> S({0, 1, 1, 2, 3, 5, 8, 13});
-	vector<num> tr = BerlekampMassey(S);
-	REQUIRE(tr == vector<num>({num(1), num(1)}));
-	num res = linearRec(S, tr, 1000);
-	REQUIRE(res == num(517691607));
+int guess_nth_term(vector<int> x, lint n) {
+	if (n < x.size()) return x[n];
+	vector<int> v = berlekamp_massey(x);
+	if (v.empty()) return 0;
+	return get_nth(v, x, n);
 }
