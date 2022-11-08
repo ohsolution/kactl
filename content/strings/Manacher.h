b353655a1920b4bc5c10407e13e4f7bd9b6ed827
ohhaeseong <43700074+ohsolution@users.noreply.github.com>
2022-11-08 16:17:59 +0900

    Update Manacher.h

--- content/strings/Manacher.h
+++ content/strings/Manacher.h
@@ -1,23 +1,29 @@
-/**
- * Author: User adamant on CodeForces
- * Source: http://codeforces.com/blog/entry/12143
- * Description: For each position in a string, computes p[0][i] = half length of
- *  longest even palindrome around pos i, p[1][i] = longest odd (half rounded down).
- * Time: O(N)
- * Status: Stress-tested
- */
-#pragma once
+int cir[max_v];
 
-array<vi, 2> manacher(const string& s) {
-	int n = sz(s);
-	array<vi,2> p = {vi(n+1), vi(n)};
-	rep(z,0,2) for (int i=0,l=0,r=0; i < n; i++) {
-		int t = r-i+!z;
-		if (i<r) p[z][i] = min(t, p[z][l+t]);
-		int L = i-p[z][i], R = i+p[z][i]-!z;
-		while (L>=1 && R+1<n && s[L-1] == s[R+1])
-			p[z][i]++, L--, R++;
-		if (R>r) l=L, r=R;
-	}
-	return p;
+int main()
+{
+    string t; ci(t);
+
+    string s = "#";
+    reverse(t.begin(), t.end());
+    while (t.size()) s += t.back(), s += '#', t.pop_back();
+
+    int n = s.size();
+
+    auto mana = [&]()
+    {
+        int r = 0, p = 0;
+        fa(i,0,n)
+        {
+            cir[i] = i <= r ? min(cir[2*p-i],r-(int)i): 0;
+
+            while (i - cir[i] - 1 >= 0 && i + cir[i] + 1 < n && s[i - cir[i] - 1] == s[i + cir[i] + 1]) ++cir[i];
+
+            if (r < i + cir[i]) r = i + cir[i], p = i;            
+        }
+    };
+
+    mana();    
+    fa(i, 1, n-1) co(cir[i] << " ");
+    return 0;
 }
